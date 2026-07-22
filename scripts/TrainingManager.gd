631f003c698ab16e650a16e26cdca2b466252bf4
Neo <lzt777@163.com>
2026-07-22 22:17:11 +0800

    play mode ai control

--- scripts/TrainingManager.gd
+++ scripts/TrainingManager.gd
@@ -35,6 +35,7 @@ var pending_training_samples:Array = []
 
 var Agent: AIAgent
 var isAIMode : bool = false
+var isPlayMode : bool = false
 var ep:int = 1
 
 func _enter_tree():
@@ -48,25 +49,60 @@ func _ready() -> void:
 		isAIMode = true
 		if GameManager.instance.game_mode == GameManager.GameMode.Play:
 			Agent.set_mode(AIAgent.AIAgentMode.INFERENCE)
+			isPlayMode = true
 		else:
 			Agent.set_mode(AIAgent.AIAgentMode.TRAINING)
+			isPlayMode = false
 
 func _physics_process(_delta: float) -> void:
 	if not isAIMode:
 		return
-	if Input.is_key_pressed(KEY_P):
+	if ! isPlayMode && Input.is_key_pressed(KEY_P):
 		_request_policy_save()
 	ai_loop()
 
 func ai_loop() -> void:
-	_run_training_collection_loop()
-
-func _run_training_collection_loop() -> void:
 	var now = Time.get_ticks_msec()
 	if now <= next_training_sample_time_ms:
 		return
-
+	
 	next_training_sample_time_ms = now + int(GameManager.instance.game_settings.ai_update_interval * 1000)
+	
+	if isPlayMode:
+		if not GameManager.instance.training_fields.is_empty():
+			var field = GameManager.instance.training_fields[0]
+			var field_id = field.id
+			if not GameData.game_end[field_id]:
+				var sensor_data = field.player.get_sensor_data()
+				var operations : PackedFloat32Array = Agent.ProcessSensorData(sensor_data, false)
+				var horizon :float = operations[0]
+				var vertical:float = operations[1]
+				var angle_x:float = operations[2]
+				var angle_y :float = operations[3]
+				var shoot :float = operations[4]
+				
+				var player = field.player
+				if not is_instance_valid(player):
+					return
+				if not GameData.player_input.has(player.id):
+					return
+
+				var input_state = GameData.player_input[player.id] as GameData.PlayerInputState
+				var move_dir :Vector2 = Vector2(horizon, vertical)
+				if move_dir.length() > 0:
+					input_state.start_move(move_dir)
+				else:
+					input_state.stop_move()
+
+				input_state.shooting = shoot > 0
+				input_state.aim_direction = Vector2(angle_x, angle_y).normalized()
+			else:
+				var sensor_data = field.player.player_sensor_data_cache
+				Agent.ProcessSensorData(sensor_data, true)
+	else:
+		_run_training_collection_loop()
+
+func _run_training_collection_loop() -> void:		
 	for field in GameManager.instance.training_fields.values():
 		_collect_training_sample(field)
 
